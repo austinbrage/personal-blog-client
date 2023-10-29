709e48831dd54277f3641d23c1e9eda9a9e01dcd
agusb19 <agustinbrage19@gmail.com>
2023-10-29 01:45:32 -0300

    feat: add user service for api requests

---
+++ src/services/users.ts
@@ -0,0 +1,106 @@
+// import { API_URL } from '../utils/config'
+// const USER_URL = new URL('/user', API_URL)
+import { type IUser, UserInfo, UserResponse } from '../types/users'
+
+export class User implements IUser {
+    url: string
+
+    constructor(USER_URL: string) {
+        this.url = USER_URL
+    }
+
+    getData = async () => {
+        const url = new URL('/data', this.url)
+
+        const response = await fetch(url)
+        return await response.json()
+    }
+
+    validate = async ({ name, password }: UserInfo['credentials']) => {
+        const url = new URL('/login', this.url)
+
+        url.searchParams.append('name', name)
+        url.searchParams.append('password', password)
+
+        const response = await fetch(url)
+        return await response.json() as UserResponse['noData']
+    }
+
+    insertNew = async ({ name, password, email, author }: UserInfo['data']) => {
+        const url = new URL('/register', this.url)
+
+        const options = {
+            method: 'POST',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify({ name, password, email, author })
+        }
+
+        const response = await fetch(url, options)
+        return await response.json() as UserResponse['noData']
+    }
+
+    changeName = async ({ id, name }: UserInfo['idName']) =>  {
+        const url = new URL('/name', this.url)
+
+        const options = {
+            method: 'PATCH',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify({ id, name })
+        }
+
+        const response = await fetch(url, options)
+        return await response.json() as UserResponse['noData']
+    }
+
+    changeEmail = async ({ id, email }: UserInfo['idEmail']) =>  {
+        const url = new URL('/email', this.url)
+
+        const options = {
+            method: 'PATCH',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify({ id, email })
+        }
+
+        const response = await fetch(url, options)
+        return await response.json() as UserResponse['noData']
+    }
+
+    changeAuthor = async ({ id, author }: UserInfo['idAuthor']) =>  {
+        const url = new URL('/author', this.url)
+
+        const options = {
+            method: 'PATCH',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify({ id, author })
+        }
+
+        const response = await fetch(url, options)
+        return await response.json() as UserResponse['noData']
+    }
+
+    changePassword = async ({ id, password }: UserInfo['idPassword']) =>  {
+        const url = new URL('/password', this.url)
+
+        const options = {
+            method: 'PATCH',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify({ id, password })
+        }
+
+        const response = await fetch(url, options)
+        return await response.json() as UserResponse['noData']
+    }
+
+    removeData = async ({ id }: UserInfo['id']) => {
+        const url = new URL('/data', this.url)
+        
+        const options = {
+            method: 'DELETE',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify({ id })
+        }
+
+        const response = await fetch(url, options)
+        return await response.json() as UserResponse['noData']
+    }
+}
