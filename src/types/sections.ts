3adefc5487b179d2ed6a948aac754b73173d02cc
agusb19 <agustinbrage19@gmail.com>
2023-10-29 21:28:54 -0300

    feat: add types for section service

---
+++ src/types/sections.ts
@@ -0,0 +1,52 @@
+import { type APIResponse } from "./api"
+
+type Data = {
+    content: string 
+    font_size: string 
+    font_weight: string 
+    font_family: string 
+    line_height: string 
+    margin_top: string 
+    text_align: string 
+    text_color: string
+}
+
+export type SectionInfo = {
+    id: { id: number }
+    idData: { id: number } & Data
+    articleId: { article_id: string } //Type of string due to the searchParams.append functionality
+    articleIdData: { article_id: number } & Data
+}
+
+export type SectionResponse = {
+    noData: APIResponse<null>
+    data: APIResponse< SectionInfo['idData'] > // It doesn't contemplate the sequence field
+}
+
+export interface ISection {
+    url: URL
+    getData: ({ article_id }: SectionInfo['articleId']) => Promise< SectionResponse['data'] >
+    changeAll: ({
+        id,
+        content,
+        font_size,
+        font_weight,
+        font_family,
+        line_height,
+        margin_top,
+        text_align,
+        text_color
+    }: SectionInfo['idData']) => Promise< SectionResponse['noData'] >
+    inserNew: ({
+        article_id,
+        content,
+        font_size,
+        font_weight,
+        font_family,
+        line_height,
+        margin_top,
+        text_align,
+        text_color
+    }: SectionInfo['articleIdData']) => Promise< SectionResponse['noData'] >
+    removeData: ({ id }: SectionInfo['id']) => Promise< SectionResponse['noData'] >
+}
