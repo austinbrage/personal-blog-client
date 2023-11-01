29f33a2517ca8fae882e140e08dc0867e4db52b0
agusb19 <agustinbrage19@gmail.com>
2023-11-01 20:12:50 -0300

    feat: export api url directly (without env secret)

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,5 @@
-import 'dotenv/config'
+export const API_URL = 'http://localhost:3000/personal-blog'
 
-export const API_URL = process.env.NODE_ENV !== 'production' 
-    ? process.env.BASE_API_TEST_URL
-    : process.env.BASE_API_PROD_URL
+// export const API_URL = import.meta.env.MODE !== 'production' 
+//     ? import.meta.env.BASE_API_TEST_URL
+//     : import.meta.env.BASE_API_PROD_URL
