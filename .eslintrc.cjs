c96c3f0bec837dc93685c7cd373c0d6973d9b977
agusb19 <agustinbrage19@gmail.com>
2023-11-01 10:48:39 -0300

    chore: add and config tailwindcss package

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules', "tailwind.config.js", "postcss.config.js"],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
