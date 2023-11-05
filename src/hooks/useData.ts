222084aec386f1a5aacf730a1893364422f75ed5
agusb19 <agustinbrage19@gmail.com>
2023-11-05 00:04:09 -0300

    feat: return refetch method and sucess state

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,43 +1,59 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { type APIResponse } from '../types/api'
 
 export const useData = <T>(request: Promise<APIResponse<T>>) => {
     const [data, setData] = useState<T[] | []>([])
     const [message, setMessage] = useState<string>('')
     const [hasFail, setHasFail] = useState<string>('')
-    const [hasError, setHasError] = useState<unknown>()
+    const [success, setSuccess] = useState<boolean>(false)
+    const [hasError, setHasError] = useState<unknown>(null)
     const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [hasAttempted, setHasAttempted] = useState<boolean>(false)
 
-    useEffect(() => {
-        setHasError(false)
+    const makeRequest = useCallback(() => {
+        setHasFail('')
+        setSuccess(false)
+        setHasError(null)
         setIsLoading(true)
 
         request
             .then(res => {
                 if(res.success) {
+                    setSuccess(true)
                     setData(res.result.data || [])
                     setMessage(res.result.message)
-                } else if (res.error.status === 'fail'){
+                    return
+                } 
+                
+                if (res.error.status === 'fail'){
+                    setSuccess(false)
                     setHasFail(res.error.message)
-                } else {
-                    process.env.NODE_ENV !== 'production' 
-                        && console.log(res.error.message)
-                    throw new Error('Internal Server Error')
+                    return
                 }
+
+                process.env.NODE_ENV !== 'production' && console.log(res.error.message)
+                throw new Error('Internal Server Error')
             })
             .catch(err => {
                 setHasError(err)
             })
             .finally(() => {
                 setIsLoading(false)
+                setHasAttempted(true)
             })
     }, [request])
 
+    useEffect(() => {
+        hasAttempted || makeRequest()
+    }, [makeRequest, hasAttempted])
+
     return {
         data,
+        success,
         message,
         hasFail,
         hasError,
-        isLoading
+        isLoading,
+        reFetch: makeRequest
     }
 }
