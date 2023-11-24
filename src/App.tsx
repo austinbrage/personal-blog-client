5c1c7706019d4a33d9f07d00b9822c4a64fdc8b6
agusb19 <agustinbrage19@gmail.com>
2023-11-24 15:11:18 -0300

    feat: add routed home and editor pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { toastConfig } from './utils/config'
 import { HomePage } from './pages/Home'
+import { EditorPage } from './pages/Editor'
 import { Toaster } from 'react-hot-toast'
 import './App.css'
 
@@ -8,9 +10,20 @@ function App() {
 
   const queryClient = new QueryClient()
 
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <HomePage/>
+    },
+    {
+      path: '/dashboard',
+      element: <EditorPage/>
+    }
+  ])
+
   return (
     <QueryClientProvider client={queryClient}>
-      <HomePage/>
+      <RouterProvider router={router}/>
       <Toaster toastOptions={toastConfig}/>
     </QueryClientProvider>
   )
