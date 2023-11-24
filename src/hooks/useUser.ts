e377142dea5cc5b235fb368cb31ce41b7e9b0666
agusb19 <agustinbrage19@gmail.com>
2023-11-24 15:32:52 -0300

    feat: add route redirection when user registered

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -42,6 +42,10 @@ export const useValidation = () => {
 
 export const useRegister = () => {
 
+    const navigate = useNavigate()
+    const updateUserToken = useAPIStore(state => state.updateUserToken)
+    const updateUserSession = useAPIStore(state => state.updateUserSession)
+
     const { mutate, isPending } = useMutation({
         mutationKey: ['user', 'register'],
         mutationFn: useService.insertNew,
@@ -54,8 +58,17 @@ export const useRegister = () => {
         },
         onSuccess: async (data) => {
             data.success
-                ? toast.success(`Api message: ${data.result.message}`, { id: TOAST_ID })
+                ? toast.success(`Api message: ${data.result.message}`, 
+                    { 
+                        id: TOAST_ID, 
+                        style: { minWidth: '360px' }
+                    }
+                )
                 : toast.error(  `Api message: ${data.error.message}`,  { id: TOAST_ID })
+
+            data.success && updateUserToken(data.result.token)
+            data.success && updateUserSession('onSession')
+            data.success && navigate('/dashboard')
         }
     })
 
