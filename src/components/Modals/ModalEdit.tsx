aad44b0b26ea5487fb2bd0ca5cf8324f3d0adc5a
agusb19 <agustinbrage19@gmail.com>
2023-11-26 12:39:55 -0300

    feat: implement article edit hook in modal edit

--- src/components/Modals/ModalEdit.tsx
+++ src/components/Modals/ModalEdit.tsx
@@ -1,16 +1,22 @@
-import { useRef, FormEvent, LegacyRef } from "react"
-import { useArticleAdd } from "../../hooks/useArticles"
+import { useAPIStore } from "../../stores/api"
+import { useRef, useState, useEffect, FormEvent, RefObject } from "react"
+import { useArticleEdit } from "../../hooks/useArticles"
 
 type Props = {
-    mode: 'add' | 'edit'
-    modalRef: LegacyRef<HTMLDivElement> 
+    isToggle: boolean
+    modalRef: RefObject<HTMLDivElement> 
     toggleModal: () => void
 }
 
-export function ModalEdit({ mode, modalRef, toggleModal }: Props) {
+export function ModalEdit({ isToggle, modalRef, toggleModal }: Props) {
     
-    const formRef = useRef<HTMLFormElement>(null)
-    const { addNewArticle, isPending } = useArticleAdd()
+    const formRef = useRef<HTMLFormElement>(null)    
+    const { editArticle, isPending } = useArticleEdit()
+    const articleData = useAPIStore(state => state.articleData)
+
+    const [name, setName] = useState<string>(articleData?.name ?? '')
+    const [title, setTitle] = useState<string>(articleData?.title ?? '')
+    const [description, setDescription] = useState<string>(articleData?.description ?? '')
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
 
@@ -27,11 +33,17 @@ export function ModalEdit({ mode, modalRef, toggleModal }: Props) {
             description: getStringValue('description')
         }   
 
-        if(mode === 'add') addNewArticle(newArticleData)
+        editArticle(newArticleData)
         
         toggleModal()
         formRef.current?.reset()
     }
+
+    useEffect(() => {
+        setName(articleData?.name ?? '')
+        setTitle(articleData?.title ?? '')
+        setDescription(articleData?.description ?? '')
+    }, [articleData, isToggle])
 
     return (
         <div ref={modalRef} id="crud-modal" tabIndex={-1} aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
@@ -41,7 +53,7 @@ export function ModalEdit({ mode, modalRef, toggleModal }: Props) {
 
                     <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                         <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                            Create New Article
+                            Edit article info
                         </h3>
                         <button onClick={toggleModal} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crud-modal">
                             <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -55,17 +67,34 @@ export function ModalEdit({ mode, modalRef, toggleModal }: Props) {
                         
                         <div className="grid gap-4 mb-4 grid-cols-2">
                             <div className="col-span-2">
-                                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                <label htmlFor="name2" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                     NickName
                                 </label>
-                                <input placeholder="Type articles alias name" type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required/>
+                                <input 
+                                    required
+                                    id="name2" 
+                                    type="text" 
+                                    name="name" 
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)} 
+                                    placeholder="Type articles alias name" 
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" 
+                                />
                             </div>
 
                             <div className="col-span-2">
-                                <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                <label htmlFor="title2" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                     Title
                                 </label>
-                                <input type="text" name="title" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type articles complete title" required/>
+                                <input 
+                                    required
+                                    id="title2" 
+                                    type="text" 
+                                    name="title" 
+                                    value={title}
+                                    onChange={(e) => setTitle(e.target.value)} 
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type articles complete title" 
+                                />
                             </div>
                             
                             {/* <div className="col-span-2 sm:col-span-1">
@@ -84,16 +113,25 @@ export function ModalEdit({ mode, modalRef, toggleModal }: Props) {
                             </div> */}
 
                             <div className="col-span-2">
-                                <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                <label htmlFor="description2" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                     Article Description
                                 </label>
-                                <textarea id="description" rows={4} name="description" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write article description here"></textarea>                    
+                                <textarea 
+                                    required
+                                    rows={4} 
+                                    id="description2" 
+                                    name="description" 
+                                    value={description}
+                                    onChange={(e) => setDescription(e.target.value)} 
+                                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write article description here"
+                                >
+                                </textarea>                    
                             </div>
                         </div>
 
                         <button type="submit" className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             <svg className="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
-                            Add new article
+                            Edit article
                         </button>
 
                     </form>
