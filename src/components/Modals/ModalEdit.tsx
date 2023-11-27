628733980d7cfc876a2abf80fb5421a95c811268
agusb19 <agustinbrage19@gmail.com>
2023-11-27 01:46:48 -0300

    feat: change id element to be unique

--- src/components/Modals/ModalEdit.tsx
+++ src/components/Modals/ModalEdit.tsx
@@ -46,7 +46,7 @@ export function ModalEdit({ isToggle, modalRef, toggleModal }: Props) {
     }, [articleData, isToggle])
 
     return (
-        <div ref={modalRef} id="crud-modal" tabIndex={-1} aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div ref={modalRef} id="crud-modal-2" tabIndex={-1} aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
             
             <div className="relative p-4 w-full max-w-md max-h-full">
                 <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
