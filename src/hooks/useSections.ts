61eda8a3dc5140f3217863048316876087cf86b4
agusb19 <agustinbrage19@gmail.com>
2023-11-27 20:52:48 -0300

    feat: add hook to delete section content, styles

--- src/hooks/useSections.ts
+++ src/hooks/useSections.ts
@@ -1,13 +1,13 @@
 import toast from "react-hot-toast"
 import React, { useMemo, useEffect } from "react"
-import { useQuery } from "@tanstack/react-query"
 import { Section } from "../services/sections"
 import { useAPIStore } from "../stores/api"
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query"
 import { type processedData } from "../types/sections"
 
 const sectionService = new Section()
 const TOAST_ID_QUERY = 'SECTION_TOAST_QUERY'
-// const TOAST_ID_MUTATE = 'SECTION_TOAST_MUTATE'
+const TOAST_ID_MUTATE = 'SECTION_TOAST_MUTATE'
 
 export const useSectionData = () => {
     
@@ -72,5 +72,46 @@ export const useSectionData = () => {
 
     return {
         sectionData: dataResult
+    }
+}
+
+export const useSectionDelete = () => {
+
+    const queryClient = useQueryClient()
+    const userToken = useAPIStore(state => state.userToken)
+    const sectionId = useAPIStore(state => state.sectionId)
+
+    const { mutate, isPending } = useMutation({
+        mutationKey: ['section', 'delete'],
+        mutationFn: sectionService.removeData,
+
+        onMutate: () => {
+            toast.loading('Requesting API', { id: TOAST_ID_MUTATE })
+        },
+        onError: () => {
+            toast.error('Internal error, please try again', { id: TOAST_ID_MUTATE })
+        },
+        onSuccess: async (data) => {
+            data.success
+                ? toast.success(
+                    `Api message: ${data.result.message}`, 
+                    { 
+                        id: TOAST_ID_MUTATE, 
+                        style: { minWidth: '500px' } 
+                    }
+                )
+                : toast.error(`Api message: ${data.error.message}`,  { id: TOAST_ID_MUTATE })
+
+            data.success && queryClient.invalidateQueries({ queryKey: ['section', 'data'] })
+        }
+    })
+
+    const deleteSection = () => {
+        mutate({ token: userToken, id: sectionId })
+    }
+
+    return {
+        deleteSection,
+        isPending
     }
 }
