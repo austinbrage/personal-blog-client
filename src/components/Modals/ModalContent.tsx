844bc989874b73e7876799669ffb3faeac05ffad
agusb19 <agustinbrage19@gmail.com>
2023-11-29 21:04:16 -0300

    feat: add pair modal to choose style section data

--- src/components/Modals/ModalContent.tsx
+++ src/components/Modals/ModalContent.tsx
@@ -1,5 +1,6 @@
 import { useAPIStore } from "../../stores/api"
 import { useEscape } from "../../hooks/useCommands"
+import { SelectOption } from "../Dropdowns/SelectOption"
 import { useState, useEffect, type RefObject, type FormEvent } from "react"
 import Draggable, { type DraggableEvent, type DraggableData } from 'react-draggable'
 
@@ -15,10 +16,14 @@ export function ModalContent({ mode, modalRef }: Props) {
     const sectionData = useAPIStore(state => state.sectionData)
 
     const [name, setName] = useState<string>('')
-    const [position, setPosition] = useState<Position>({ x: 0, y: 0 })
+    const [position1, setPosition1] = useState<Position>({ x: 0, y: 0 })
+    const [position2, setPosition2] = useState<Position>({ x: 0, y: 0 })
     
-    const handleDrag = (_e: DraggableEvent, { deltaX, deltaY }: DraggableData) => {
-        setPosition(prevPos => ({ x: prevPos.x + deltaX, y: prevPos.y + deltaY }))
+    const handleDrag1 = (_e: DraggableEvent, { deltaX, deltaY }: DraggableData) => {
+        setPosition1(prevPos => ({ x: prevPos.x + deltaX, y: prevPos.y + deltaY }))
+    }
+    const handleDrag2 = (_e: DraggableEvent, { deltaX, deltaY }: DraggableData) => {
+        setPosition2(prevPos => ({ x: prevPos.x + deltaX, y: prevPos.y + deltaY }))
     }
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
@@ -55,9 +60,9 @@ export function ModalContent({ mode, modalRef }: Props) {
             className="flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
         >        
             <Draggable 
-                onDrag={handleDrag} 
+                onDrag={handleDrag1} 
                 handle=".modal-header-1"
-                defaultPosition={position}
+                defaultPosition={position1}
             >
                 <div className="relative p-4 w-full max-w-md max-h-full">
                     <div className="relative rounded-lg shadow backdrop-blur-sm bg-[rgba(55,65,65,0.8)]">
@@ -100,6 +105,67 @@ export function ModalContent({ mode, modalRef }: Props) {
                             </button>
 
                         </form>
+
+                    </div>
+                </div>
+
+            </Draggable>
+
+            <Draggable 
+                onDrag={handleDrag2} 
+                handle=".modal-header-2"
+                defaultPosition={position2}
+            >
+                <div className="relative p-4 w-full max-w-lg max-h-full">
+                    <div className="relative h-60 rounded-lg shadow backdrop-blur-sm bg-[rgba(55,65,65,0.8)]">
+
+                        <div className="modal-header-2 cursor-grab flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                                {/* {mode === 'add' ? 'New Styles' : 'Edit Styles'} */}
+                                Choose Styles
+                            </h3>
+                            <button onClick={closeModal} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crud-modal">
+                                <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                </svg>
+                                <span className="sr-only">Close modal</span>
+                            </button>
+                        </div>
+
+                        <div className='h-32 overflow-y-auto'>
+                            <div className='flex items-center justify-around gap-2 mt-2 ms-3 z-20'>
+                                <p className="font-semibold text-lg tracking-wider text-white">
+                                    Pick a text color
+                                </p>
+                                <SelectOption
+                                    label="Select color"
+                                    setOption={() => {}}
+                                    options={['white', 'black', 'gray']}
+                                />
+                            </div>
+
+                            <div className='flex items-center justify-around gap-2 mt-2 ms-3'>
+                                <p className="font-semibold text-lg tracking-wider text-white">
+                                    Pick a text font
+                                </p>
+                                <SelectOption
+                                    label="Select font"
+                                    setOption={() => {}}
+                                    options={['sans-serif', 'monospace']}
+                                />
+                            </div>  
+
+                            <div className='flex items-center justify-around gap-2 mt-2 ms-3'>
+                                <p className="font-semibold text-lg tracking-wider text-white">
+                                    Pick a text style
+                                </p>
+                                <SelectOption
+                                    label="Select style"
+                                    setOption={() => {}}
+                                    options={['normal', 'italic']}
+                                />
+                            </div>  
+                        </div>
 
                     </div>
                 </div>
