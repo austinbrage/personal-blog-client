c5e38b93e3342771b8e6841c12aca64afe5eed49
agusb19 <agustinbrage19@gmail.com>
2023-12-14 21:51:08 -0300

    feat: add width, height, radius options

--- src/enums/general.ts
+++ src/enums/general.ts
@@ -1,40 +1,52 @@
 import { sizeOptions } from "./size"
 import { lineOptions } from "./line"
 import { alignOptions } from "./align"
+import { widthOptions } from './width'
+import { heightOptions } from './height'
 import { familyOptions } from "./family"
 import { colorPalette1 } from "./color"
 import { weightOptions } from "./weight"
 import { marginOptions } from './margin'
+import { radiusOptions } from './radius'  
 import type { ContentStyles, ContentType, Styles } from "../types/sections"
 
 const title: Styles = {
-    color:      colorPalette1[0][0],
-    fontSize:   sizeOptions["text-4xl"], 
-    marginTop:  marginOptions['m-2'], 
-    textAlign:  alignOptions['left'], 
-    fontWeight: weightOptions['semibold'], 
-    fontFamily: familyOptions['Arial'], 
-    lineHeight: lineOptions['lead-7']
+    color:        colorPalette1[0][0],
+    width:        widthOptions['w-auto'],
+    height:       heightOptions['h-auto'],
+    fontSize:     sizeOptions["text-4xl"], 
+    marginTop:    marginOptions['m-2'], 
+    textAlign:    alignOptions['left'], 
+    fontWeight:   weightOptions['semibold'], 
+    fontFamily:   familyOptions['Arial'], 
+    lineHeight:   lineOptions['lead-7'],
+    borderRadius: radiusOptions['none']
 }
 
 const subtitle: Styles = {
-    color:      colorPalette1[0][0],
-    fontSize:   sizeOptions["text-3xl"], 
-    marginTop:  marginOptions['m-2'], 
-    textAlign:  alignOptions['left'], 
-    fontWeight: weightOptions['medium'], 
-    fontFamily: familyOptions['Arial'], 
-    lineHeight: lineOptions['lead-6']
+    color:        colorPalette1[0][0],
+    width:        widthOptions['w-auto'],
+    height:       heightOptions['h-auto'],
+    fontSize:     sizeOptions["text-3xl"], 
+    marginTop:    marginOptions['m-2'], 
+    textAlign:    alignOptions['left'], 
+    fontWeight:   weightOptions['medium'], 
+    fontFamily:   familyOptions['Arial'], 
+    lineHeight:   lineOptions['lead-6'],
+    borderRadius: radiusOptions['none']
 }
 
 const paragraph: Styles = {
-    color:      colorPalette1[0][0],
-    fontSize:   sizeOptions["text-lg"], 
-    marginTop:  marginOptions['m-2'], 
-    textAlign:  alignOptions['left'], 
-    fontWeight: weightOptions['normal'], 
-    fontFamily: familyOptions['Arial'], 
-    lineHeight: lineOptions['lead-4']
+    color:        colorPalette1[0][0],
+    width:        widthOptions['w-auto'],
+    height:       heightOptions['h-auto'],
+    fontSize:     sizeOptions["text-lg"], 
+    marginTop:    marginOptions['m-2'], 
+    textAlign:    alignOptions['left'], 
+    fontWeight:   weightOptions['normal'], 
+    fontFamily:   familyOptions['Arial'], 
+    lineHeight:   lineOptions['lead-4'],
+    borderRadius: radiusOptions['none']
 }
 
 export const generalOptions: { name: ContentType, value: Styles }[] = [
