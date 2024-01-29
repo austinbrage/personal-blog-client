c326b2b2e7e004aa6e9660142626564936f5a6f6
agusb19 <agustinbrage19@gmail.com>
2024-01-29 10:15:47 -0300

    feat: add pagination in articles menu table

--- src/components/MenuTable/index.tsx
+++ src/components/MenuTable/index.tsx
@@ -1,13 +1,36 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { panelTour } from "../../utils/driver"
 import { useNavigate } from 'react-router-dom'
 import { useMenuTableCommands } from '../../hooks/useCommands'
+import { BiSolidRightArrow, BiSolidLeftArrow } from "react-icons/bi"
 import './MenuTable.css'
 import '../../driver.css'
 
 type Props = {
     postsList: string[]
     toggleModal: () => void
+}
+
+const useArticlesPagination = (postsLists: string[]) => {
+
+    const perPage = 3
+    const totalPages = Math.ceil(postsLists.length / perPage)
+    const [currentPage, setCurrentPage] = useState<number>(1)
+    const [mustPaginate, setMustPaginate] = useState<boolean>(false)
+    
+    const goNextPage = () => setCurrentPage(prev => prev + 1)
+    const goPrevPage = () => setCurrentPage(prev => prev - 1)
+
+    const hasNext = () => currentPage < totalPages
+    const hasPrev = () => currentPage > 1
+
+    useEffect(() => {
+        postsLists.length > perPage
+            ? setMustPaginate(true)
+            : setMustPaginate(false)
+    }, [postsLists, setMustPaginate])
+
+    return { currentPage, mustPaginate, goNextPage, goPrevPage, hasNext, hasPrev, perPage, totalPages }
 }
 
 export function MenuTable({ postsList, toggleModal }: Props) {
@@ -36,7 +59,18 @@ export function MenuTable({ postsList, toggleModal }: Props) {
     }
 
     useMenuTableCommands({ menuRef: menuTable, closeMenu, openMenu })
-    
+
+    const { 
+        mustPaginate, 
+        currentPage,
+        totalPages, 
+        goNextPage, 
+        goPrevPage, 
+        hasNext, 
+        hasPrev,
+        perPage
+    } = useArticlesPagination(postsList)
+
     return (
         <div className='menu-table'>
             <div className="navigation" ref={menuTable}>
@@ -53,26 +87,85 @@ export function MenuTable({ postsList, toggleModal }: Props) {
                         <button type='button'>CREATE ARTICLE</button>
                     </li>
                     
-                    {postsList.map((post, index) => ( 
-                        <li 
-                            key={post}
-                            style={{'--i': index + 1} as React.CSSProperties} 
-                            onClick={() => changePathroute({ type: 'edit', article: post })} 
-                        >
-                            <button type='button'>{post}</button>
-                        </li>
-                    ))}
-                    
+                    {postsList.map((post, index) => { 
+                        const calc = (index + 1) - ((currentPage - 1) * perPage) 
+
+                        if(mustPaginate && (index + 1) > perPage * currentPage) return
+                        if(mustPaginate && index < perPage * (currentPage - 1)) return
+
+                        return (
+                            <li 
+                                key={post}
+                                style={{'--i': mustPaginate ? calc : index + 1} as React.CSSProperties} 
+                                onClick={() => changePathroute({ type: 'edit', article: post })} 
+                            >
+                                <button type='button'>{post}</button>
+                            </li>
+                        ) 
+                    })}
+
+                    {(mustPaginate && currentPage === totalPages && postsList.length !== totalPages * perPage) && (
+                        [...Array((totalPages * perPage) - postsList.length)].map((_, index) => index).reverse().map((elem) => (
+                            <li 
+                                key={elem}
+                                style={{'--i': perPage - elem} as React.CSSProperties}
+                            >
+                                <button 
+                                    type='button' 
+                                    className='!text-[#242424] pointer-events-none'
+                                >
+                                        Non-used text
+                                </button>
+                            </li>
+                        ))
+                    )}
+
                     <li 
                         onClick={() => changePathroute({ type: 'profile' })}
-                        style={{'--i': postsList.length + 1} as React.CSSProperties}
+                        style={{'--i': mustPaginate ? perPage + 1 : postsList.length + 1} as React.CSSProperties}
                     >
-                        <button type='button'>User profile</button>
+                        <button type='button'>[ User profile ]</button>
                     </li>
+
+                    {mustPaginate && (
+                        <li
+                            className='flex'
+                            style={{'--i': perPage + 2} as React.CSSProperties}
+                        >
+                            <button 
+                                type='button'
+                                disabled={!hasPrev()}
+                                onClick= {() => goPrevPage()} 
+                                className={`
+                                    !grid place-content-center 
+                                    ${hasPrev() 
+                                        ? 'pointer-events-auto text-[#848181] bg-[#242424]' 
+                                        : 'pointer-events-none !text-[#343434] !bg-[#1D1D1F]'
+                                    }
+                                `}
+                            >
+                                <BiSolidLeftArrow/>
+                            </button>
+                            <button 
+                                type='button' 
+                                disabled={!hasNext()}
+                                onClick= {() => goNextPage()} 
+                                className={`
+                                    !grid place-content-center 
+                                    ${hasNext() 
+                                        ? 'pointer-events-auto text-[#848181] bg-[#242424]' 
+                                        : 'pointer-events-none !text-[#343434] !bg-[#1D1D1F]'
+                                    }
+                                `}
+                            >
+                                <BiSolidRightArrow/>
+                            </button>
+                        </li>
+                    )}
 
                     <li 
                         className='flex'
-                        style={{'--i': postsList.length + 2} as React.CSSProperties}
+                        style={{'--i': mustPaginate ? perPage + 3 : postsList.length + 2} as React.CSSProperties}
                     >
                         <button type='button' onClick={() => panelTour.drive()}>Guide</button>
                         <button type='button' onClick={toggleModal}>shortcuts</button>
