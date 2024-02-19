9cb8beba53a8d08621e82722102738b03522f361
agusb19 <agustinbrage19@gmail.com>
2024-02-19 14:23:28 -0300

    feat: create post card on article image, description, keywords

--- src/components/Sections/index.tsx
+++ src/components/Sections/index.tsx
@@ -54,28 +54,45 @@ export function Sections({ newData, currentArticle }: Props) {
                 <div className='border-2 ms-5 border-dashed bg-gray-800 border-gray-700'>
                 </div>
 
-                <h4 className="ms-5 mt-5 text-5xl font-bold font-dm-sans tracking-wider">
-                    {currentArticle.title}
-                </h4>
-                
-                {currentArticle.image ? (
-                    <div className='flex'>
+                <div className="flex flex-col items-center justify-center w-full px-10 mt-5 text-center">
+                   
+                    <h4 className="ms-5 mt-5 text-5xl font-bold font-dm-sans tracking-wider">
+                        {currentArticle.title}
+                    </h4>
+                    
+                    <div className='relative mt-5 w-full lg:w-5/6 rounded-md'>
                         <img 
-                            src={currentArticle.image} 
-                            alt={`${currentArticle.name} image post`} 
-                            style={{ aspectRatio: '16/9' }}
-                            className='ms-10 my-5 w-80 rounded-md'
+                            src="https://th.bing.com/th/id/OIP.rXaXp7QU8INIhQWoxnZYlgHaEo?rs=1&pid=ImgDetMain" 
+                            alt="Post card background image"
+                            className='absolute top-0 left-0 w-full h-3/4 opacity-80' 
                         />
-                        <p className="ms-10 mt-5 mb-10 text-2xl font-semibold">
-                            {currentArticle.description}
-                        </p>
+                        <div className='w-full backdrop-blur-md bg-[rgba(32,32,37,0.5)]'>
+                            <div className='flex flex-col md:flex-row items-center'>
+                                {currentArticle.image && (
+                                    <img 
+                                        src={currentArticle.image} 
+                                        alt={`${currentArticle.name} image post`} 
+                                        style={{ aspectRatio: '16/9' }}
+                                        className='ms-10 my-5 w-80 rounded-md'
+                                    />
+                                )}
+                                <p className="ms-10 mt-5 mb-10 line-clamp-6 font-dm-sans font-semibold text-xl text-start text-[rgb(229,231,235)]">
+                                    {currentArticle.description}
+                                </p>
+                            </div>
+                            <div className='flex gap-5 items-center justify-end me-4 mb-4'>
+                                {currentArticle.keywords.split(',').map(elem => (
+                                    <span key={elem} className='py-2 px-3 rounded-lg font-dm-sans  text-md tracking-wide bg-gradient-to-r from-stone-700 to-stone-900'>
+                                        {elem}
+                                    </span>
+                                ))}
+                            </div>
+
+                        </div>
                     </div>
-                ) : (
-                    <p className="ms-10 mt-5 mb-10 text-2xl font-semibold">
-                        {currentArticle.description}
-                    </p>
-                )}
-                
+
+                </div>
+ 
                 <SectionList
                     newData={newData}
                     editData={editSection}
