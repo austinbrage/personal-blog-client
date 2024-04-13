a306fe525af8f451301ac8155dd86a23266fd5cf
agusb19 <agustinbrage19@gmail.com>
2024-04-13 18:12:06 -0300

    feat: fix general tab to treat image_url and image_s3 as image

--- src/components/Tabs/General.tsx
+++ src/components/Tabs/General.tsx
@@ -1,6 +1,11 @@
 import { generalOptions } from "../../enums/general"
 import type { Styles, ContentType } from "../../types/sections"
 
+type CompareTypes = { 
+    currentContentType: ContentType, 
+    optionName: ContentType 
+}
+
 type NewData = {
     newStyles: Styles
     newContentType: ContentType
@@ -12,6 +17,12 @@ type Props = {
 }
 
 export function GeneralTab({ currentContentType, changeStyles }: Props) {
+
+    const handleCompareTypes = ({ currentContentType, optionName }: CompareTypes) => {
+        if(currentContentType.includes('image') && optionName.includes('image')) return true
+        if(currentContentType === optionName) return true
+        return false
+    }
 
     return (
         <div className="absolute top-5 left-5 grid grid-cols-[1fr,1fr] gap-2">
@@ -25,11 +36,15 @@ export function GeneralTab({ currentContentType, changeStyles }: Props) {
                             newContentType: option.name 
                         })}
                         className={`
-                            ${(currentContentType === option.name) ? 'opacity-100 scale-105' : 'opacity-70 scale-100'}
+                            ${
+                                handleCompareTypes({ currentContentType, optionName: option.name })
+                                    ? 'opacity-100 scale-105' 
+                                    : 'opacity-70 scale-100'
+                            }
                             p-3 w-36 text-center text-xl font-semibold cursor-pointer rounded-md backdrop-blur-md text-white bg-[rgba(0,0,0,0.3)] transition-all hover:opacity-100 hover:scale-105
                         `.trim()}
                     >
-                        {option.name}
+                        {option.name === 'image_url' ? 'image' : option.name}
                     </div>
                 ))   
             }
